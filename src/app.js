396137880c4328fcd4a1e123bcb41c6ea4cbc543
Melody Callejas <154374405+melodycious@users.noreply.github.com>
2024-02-23 17:56:05 +0000

    comit prueba

--- src/app.js
+++ src/app.js
@@ -5,7 +5,50 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const topSuit = document.getElementById("topSuit");
+const number = document.getElementById("number");
+const bottomSuit = document.getElementById("bottomSuit");
+
+function cardGenerator() {
+  let cardNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let cardSuit = ["♦", "♥", "♠", "♣"];
+  let randomNumber = Math.floor(Math.random() * cardNumber.length);
+  let randomSuit = Math.floor(Math.random() * cardSuit.length);
+}
+
+topSuit.innerText = cardSuit[randomSuit];
+bottomSuit.innerText = cardSuit[randomSuit];
+Number.innerText = cardNumber[randomNumber];
+
+window.onload = () => {
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector(".card").innerHTML = cardGenerator();
+  });
 };
+
+/* function button() {
+  let value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let suit = ["♦", "♥", "♠", "♣"];
+  let randomValue = Math.floor(Math.random() * value.length);
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  document.querySelector("#topSuit").innerHTML = suit[randomSuit];
+  document.querySelector("#bottomSuit").innerHTML = suit[randomSuit];
+  document.querySelector("#number").innerHTML = value[randomValue];
+  document.querySelector(".card").style.color =
+    randomSuit <= 1 ? "red" : "black";
+} */
